b94614db63a7587e567c1e02f496a767395ac6a7
Charles Yun <cy@Charless-MacBook-Pro.local>
2019-02-25 10:48:40 -0800

    clean and mod

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -6,8 +6,7 @@ var app = express();
 
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get("/", function(req, res) {
-  // TODO: Get all records from the DB and render to view
+router.get("/", function(req,res) {
   db.pokemon.findAll().then(function(pokemons) {
     res.render("pokemon/favorites", {pokemons});
   });
@@ -15,8 +14,7 @@ router.get("/", function(req, res) {
 
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post("/", function(req, res) {
-  // TODO: Get form data and add a new record to DB
+router.post("/", function(req,res) {
   db.pokemon.create({
     name: req.body.name
   }).then(function(pokemons) {
@@ -25,11 +23,20 @@ router.post("/", function(req, res) {
 });
 
 // GET ONE
-router.get("/:id", function(req, res) {
-  let url = "http://pokeapi.co/api/v2/pokemon/" + req.params.id;
-  request(url, function(error, response, body) {
+router.get("/:id", function(req,res) {
+  let pokemonUrl = "http://pokeapi.co/api/v2/pokemon/" + req.params.id;
+  request(pokemonUrl, function(error, response, body) {
     let data = JSON.parse(body)
     res.render("pokemon/show", {data});
+  });
+});
+
+// DELETE
+router.delete("/:id", function(req,res) {
+  db.pokemon.destroy({
+    where: {id: req.params.id}
+  }).then(function() {
+    res.redirect("/pokemon")
   });
 });
 
